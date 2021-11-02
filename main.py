683581e55aacd58685e46e6b979a0cab34036fa7
isso0424 <me@isso0424.dev>
2021-11-02 11:47:20 +0900

    feat: define neural network

--- main.py
+++ main.py
@@ -18,3 +18,35 @@ test_data = datasets.FashionMNIST(
     download=True,
     transform=ToTensor(),
 )
+
+batch_size = 64
+
+training_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+for x, y in test_dataloader:
+    print("Shape of X [N, C, H, W]", x.shape)
+    print("Shape of y: ", y.shape, y.dtype)
+    break
+
+device = "cpu"
+print("using {} device".format(device))
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        return self.linear_relu_stack(x)
+
+model = NeuralNetwork().to(device)
+print(model)
